6e1fdd9a6a478332c88744f69cbd42a76d89bc46
Maxim Serov <maxim58r@gmail.com>
2024-12-20 22:02:09 +0300

    commit checkstyle

--- src/main/java/com/chatter/chat_service/ChatServiceApplication.java
+++ src/main/java/com/chatter/chat_service/ChatServiceApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChatServiceApplication.class, args);
-	}
+    /**
+     * Entry point for the authentication service application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceApplication.class, args);
+    }
 
 }
